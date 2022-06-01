de91622792c5a339af17f963685ff486fbb51f64
rudarabello <ruda.rabello.silva@gmail.com>
2022-06-01 16:07:18 -0300

    fixing bug on habits

--- src/routes/Habits.js
+++ src/routes/Habits.js
@@ -1,6 +1,6 @@
 
 import styled from "styled-components";
-import { useEffect, useState, useContext, useRef } from "react";
+import { useEffect, useState, useContext,} from "react";
 import axios from "axios";
 import { ThreeDots } from "react-loader-spinner";
 import Header from "../components/Header";
@@ -26,131 +26,122 @@ export default function Habits() {
     const showLoading = insideSaveHabit();
     const MyHabits = renderHabits();
     const makeHabit = renderNewHabit();
-    const config = {headers: { Authorization: `Bearer ${account.token}`}};
-    const tempAxiosFunction = useRef();
-    const tempAxios2Function = useRef();
-    const axiosFunction = () => {
-        const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", config);
-        promise.then(response => setHabits(response.data));
-        promise.catch(() => alert(`Deu erro no servidor!`));}
-    const axios2Function = () => {
-        if (todayHabits.length) {
-            setPorcentageHabitsDoneToday(todayHabits.filter(habit => habit.done === true).length / todayHabits.length);
-        } else {
-            setPorcentageHabitsDoneToday(0);
-        }}
-
-    tempAxiosFunction.current = axiosFunction;
-    tempAxios2Function.current = axios2Function;
-
-    useEffect(() => {
-        tempAxiosFunction.current();
-    }, [habits]);
+    const config = { headers: { Authorization: `Bearer ${account.token}` } };
+    
+        useEffect(() => {
+            const promise = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", config);
+            promise.then(response => setHabits(response.data));
+            promise.catch(() => alert(`Deu erro no servidor!`));
+        }, [habits]);
 
 
-    useEffect(() => {
-        tempAxios2Function.current();
-    }, [habits]);
+        useEffect(() => {
+            if (todayHabits.length) {
+                setPorcentageHabitsDoneToday(todayHabits.filter(habit => habit.done === true).length / todayHabits.length);
+            } else {
+                setPorcentageHabitsDoneToday(0);
+            }
+        }, [habits]);
 
-    function renderHabits() {
-        if (habits.length > 0) {
-            return habits.map((habit, i) => <CreateHabit key={i} habit={habit} />);
-        } else {
-            return <Text>
-                Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
-            </Text>
+        function renderHabits() {
+            if (habits.length > 0) {
+                return habits.map((habit, i) => <CreateHabit key={i} habit={habit} />);
+            } else {
+                return <Text>
+                    Você não tem nenhum hábito cadastrado ainda. Adicione um hábito para começar a trackear!
+                </Text>
+            }
+
         }
 
-    }
-
-    function renderNewHabit() {
-        if (addHabit === "none") {
-            return <></>;
-        } else {
-            return <NewHabit
-                display={addHabit} opacit={opacit} inputBackgroundColor={inputBackgroundColor} >
-                <input
-                    value={newHabit.name}
-                    onChange={(e) => setNewHabit({ ...newHabit, name: e.target.value })} placeholder="nome do hábito"
-                    disabled={loading}
-                />
-                <div>
-                    {weekdays.map((day, i) => <Day key={i} index={i} day={day} newHabit={newHabit} setNewHabit={setNewHabit} loading={loading} />)}
-                </div>
-                <div>
-                    <button onClick={cancel} disabled={loading}>
-                        Cancelar
-                    </button>
-                    <button onClick={saveHabit} disabled={loading}>
-                        {showLoading}
-                    </button>
-                </div>
-            </NewHabit>;
-        }
-    }
-
-    function insideSaveHabit() {
-        if (loading) return <ThreeDots width="11.7vw" height="2.93vw" color="#FFFFFF" />;
-        return <>Salvar</>;
-    }
-
-    function cancel() {
-        setAddHabit("none");
-    }
-
-    function sucessAddinghabit(data) {
-        setHabits([...habits, data]);
-        setAddHabit("none");
-        setNewHabit({ ...newHabit, name: "", days: [] });
-        setOpacit(1);
-        setLoading(false);
-        setInputBackgroundColor("#FFFFFF");
-    }
-
-    function failureAddingHabit() {
-        alert("Erro no servidor!");
-    }
-
-    function saveHabit() {
-        if (newHabit.name && newHabit.days.length) {
-            setOpacit(0.7);
-            setLoading(true);
-            setInputBackgroundColor("#F2F2F2");
-            const config = {
-                headers: {
-                    Authorization: `Bearer ${account.token}`
-                }
-            };
-            const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", newHabit, config);
-            promise.then(response => sucessAddinghabit(response.data));
-            promise.catch(() => failureAddingHabit());
-        }
-        else {
-            alert("Preencha o nome do hábito e marque pelo menos um dia da semana!");
+        function renderNewHabit() {
+            if (addHabit === "none") {
+                return <></>;
+            } else {
+                return <NewHabit
+                    display={addHabit} opacit={opacit} inputBackgroundColor={inputBackgroundColor} >
+                    <input
+                        value={newHabit.name}
+                        onChange={(e) => setNewHabit({ ...newHabit, name: e.target.value })} placeholder="nome do hábito"
+                        disabled={loading}
+                    />
+                    <div>
+                        {weekdays.map((day, i) => <Day key={i} index={i} day={day} newHabit={newHabit} setNewHabit={setNewHabit} loading={loading} />)}
+                    </div>
+                    <div>
+                        <button onClick={cancel} disabled={loading}>
+                            Cancelar
+                        </button>
+                        <button onClick={saveHabit} disabled={loading}>
+                            {showLoading}
+                        </button>
+                    </div>
+                </NewHabit>;
+            }
         }
 
+        function insideSaveHabit() {
+            if (loading) return <ThreeDots width="11.7vw" height="2.93vw" color="#FFFFFF" />;
+            return <>Salvar</>;
+        }
+
+        function cancel() {
+            setAddHabit("none");
+        }
+
+        function sucessAddinghabit(data) {
+            setHabits([...habits, data]);
+            setAddHabit("none");
+            setNewHabit({ ...newHabit, name: "", days: [] });
+            setOpacit(1);
+            setLoading(false);
+            setInputBackgroundColor("#FFFFFF");
+        }
+
+        function failureAddingHabit() {
+            alert("Erro no servidor!");
+        }
+
+        function saveHabit() {
+            if (newHabit.name && newHabit.days.length) {
+                setOpacit(0.7);
+                setLoading(true);
+                setInputBackgroundColor("#F2F2F2");
+                const config = {
+                    headers: {
+                        Authorization: `Bearer ${account.token}`
+                    }
+                };
+                const promise = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/habits", newHabit, config);
+                promise.then(response => sucessAddinghabit(response.data));
+                promise.catch(() => failureAddingHabit());
+            }
+            else {
+                alert("Preencha o nome do hábito e marque pelo menos um dia da semana!");
+            }
+
+        }
+
+        return (
+            <Page>
+                <Header />
+                <Content>
+                    <Info>
+                        <h4>Meus hábitos</h4>
+                        <button onClick={() => setAddHabit("column")}>+</button>
+                    </Info>
+                    <HabitList>
+                        {makeHabit}
+                        {MyHabits}
+                    </HabitList>
+                </Content>
+                <Footer />
+            </Page>
+        );
+
     }
 
-    return (
-        <Page>
-            <Header />
-            <Content>
-                <Info>
-                    <h4>Meus hábitos</h4>
-                    <button onClick={() => setAddHabit("column")}>+</button>
-                </Info>
-                <HabitList>
-                    {makeHabit}
-                    {MyHabits}
-                </HabitList>
-            </Content>
-            <Footer />
-        </Page>
-    );
-
-}
-
-const Page = styled.div`
+    const Page = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -158,7 +149,7 @@ const Page = styled.div`
     width: 100%;
 `;
 
-const Content = styled.div`
+    const Content = styled.div`
     padding: 90px 4.8vw 25px 4.8vw;
     box-sizing: border-box;
     background-color: #F2F2F2;
@@ -167,7 +158,7 @@ const Content = styled.div`
     
 `;
 
-const Info = styled.div`
+    const Info = styled.div`
     width: 100%;
     height: 75px;
     box-sizing: border-box;
@@ -193,7 +184,7 @@ const Info = styled.div`
     }
 `;
 
-const HabitList = styled.div`
+    const HabitList = styled.div`
     box-sizing: border-box;
     min-height: 200px;
     display: flex;
@@ -208,7 +199,7 @@ const HabitList = styled.div`
     }
 `;
 
-const Text = styled.p`
+    const Text = styled.p`
     font-size: 4.8vw;
     line-height: 5.87vw;
     font-family: 'Lexend Deca', sans-serif;
@@ -216,7 +207,7 @@ const Text = styled.p`
     margin-top: 30px;
 `
 
-const NewHabit = styled.div`
+    const NewHabit = styled.div`
     background-color: #FFFFFF;
     width: 90.7vw;
     height: 48vw;
